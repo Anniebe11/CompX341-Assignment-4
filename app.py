da2e8112a107f4109ddf139ec009e9abb844a30c
amb97 <amb97@cms-r1-29.cms.waikato.ac.nz>
2019-06-04 15:44:21 +1200

    have no idea if anything is working as docker appears to be running something entirely seperate

--- app.py
+++ app.py
@@ -22,4 +22,33 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+
+@app.route('/isPrimeNumber/<number>')
+def is_prime_number(number):
+   # If given number is greater than 1 
+   if num > 1: 
+   # Iterate from 2 to n / 2  
+   for i in range(2, num//2): 
+       # If num is divisible by any number between  
+       # 2 and n / 2, it is not prime  
+       if (num % i) == 0: 
+           print(num, "is not prime") 
+           break
+   else: 
+       cache.append('prime', number)
+       print(num, "is prime") 
+else: 
+   print(num, "is not prime") 
+
+@app.route('/primesSaved')
+def primes_saved():
+    primes=[]
+    print()
+    while(redisClient.llen('LanguageList')!=0):
+	primeN=redisClient.lpop('LanguageList')
+        if primeN in primes:
+	else:
+            primes.append()
+    print("Stored Primes:")
+    
